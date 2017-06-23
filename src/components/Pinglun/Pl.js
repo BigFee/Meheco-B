68226dbc36a163ee7b3d17c02c003afe5ba87fab
Ann <2457551415@qq.com>
2017-06-23 11:02:21 +0800

    react-demo

---
+++ src/components/Pinglun/Pl.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 安校辉 on 2017/6/16.
+ */
+import React, { Component } from 'react';
+import { Rate } from 'antd';
+import './Pl.css';
+class Axh extends Component {
+    render() {
+        return (
+            <Rate allowHalf defaultValue={2.5} />
+        );
+    }
+}
+
+export default Axh;
